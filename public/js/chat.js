a894e3dc63f69c36cacadaa124af895cb8ee5b76
amontalvof <amontalvof92@gmail.com>
2021-05-26 20:07:47 -0500

    feat: validate token - frontend

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,29 @@
-const socket = io();
+let user = null;
+let socket = null;
+
+const url = window.location.hostname.includes('localhost')
+    ? 'http://localhost:8080/api/auth/'
+    : 'https://am-node-restserver.herokuapp.com/api/auth/';
+
+const validateJWT = async () => {
+    const token = localStorage.getItem('token') || '';
+    if (token.length <= 10) {
+        window.location = 'index.html';
+        throw new Error('There is no token on the server');
+    }
+
+    const resp = await fetch(url, { headers: { 'x-token': token } });
+
+    const { user: userDB, token: tokenDB } = await resp.json();
+    localStorage.setItem('token', tokenDB);
+    user = userDB;
+    console.log({ userDB, tokenDB });
+};
+
+const main = async () => {
+    await validateJWT();
+};
+
+main();
+
+// const socket = io();
