15725c4ffe222efce03ee26be2397473c307eb94
amontalvof <amontalvof92@gmail.com>
2021-05-29 20:19:50 -0500

    feat: list of connected users

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,12 +1,22 @@
 const { checkJWT } = require('../helpers/generateJWT');
+const ChatMessages = require('../chatMessages');
 
-const socketController = async (socket) => {
+const chatMessages = new ChatMessages();
+
+const socketController = async (socket, io) => {
     const token = socket.handshake.headers['x-token'];
     const user = await checkJWT(token);
     if (!user) {
         return socket.disconnect();
     }
-    console.log('Connected', user.name);
+    // Add connected user
+    chatMessages.connectUser(user);
+    io.emit('active-users', chatMessages.usersArr);
+
+    // clean up when a user logs out
+    socket.on('disconnect', () => {
+        chatMessages.disconnectUser(user.id);
+    });
 };
 
 module.exports = { socketController };
